37305c4a125439ba7df224e049d8f127c04037f5
nhan151206 <nhanpthe151206@fpt.edu.vn>
2023-11-08 06:37:05 +0700

    main window

--- PRN231_Project/Cinema_system/Views/Admin/Showtimemanagement/ShowtimeManagement.xaml.cs
+++ PRN231_Project/Cinema_system/Views/Admin/Showtimemanagement/ShowtimeManagement.xaml.cs
@@ -24,5 +24,32 @@ namespace Cinema_system.Views.Admin.Showtimemanagement
         {
             InitializeComponent();
         }
+
+        private bool Filter(object item)
+        {
+            if (String.IsNullOrEmpty(_FilterBox.Text))
+                return true;
+            else
+                //return ((item as MovieDTO).DisplayName.IndexOf(_FilterBox.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                return true;
+        }
+
+        private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ShowtimeListview.ItemsSource);
+            view.Filter = Filter;
+            result.Content = ShowtimeListview.Items.Count;
+            CollectionViewSource.GetDefaultView(ShowtimeListview.ItemsSource).Refresh();
+        }
+
+        private void all_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _FilterBox.Text = "";
+        }
+
+        private void _FilterBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
--- PRN231_Project/Cinema_system/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ PRN231_Project/Cinema_system/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Cinema_system {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Cinema_system;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Cinema_system;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
