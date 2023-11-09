3e3727715485e6badef47d553a417bde2d10598c
nhan151206 <nhanpthe151206@fpt.edu.vn>
2023-11-09 23:31:17 +0700

    a

---
+++ Project2023PRN231/Models/OrderDetailDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2023PRN231.Models
+{
+    public class OrderDetailDTO
+    {
+        public decimal MaHd { get; set; }
+        public string MakH { get; set; } = null!;
+        public string? TenHang { get; set; }
+        public float? Gia { get; set; }
+        public int? Soluong { get; set; }
+    }
+}
